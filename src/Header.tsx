52464c88b76e3d416e623f21e84e45c4c42ab01d
Janis <janis.meister87@gmail.com>
2023-02-19 01:32:14 +0100

    first commit

---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./assets/styles/Header.scss";
+
+export default function Header() {
+	return (
+		<header className="header">
+			<h1>TealFire</h1>
+		</header>
+	);
+}
